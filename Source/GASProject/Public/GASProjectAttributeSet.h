d0e6ad5749997ebacdb5a31c83c7b9bcb359e6a2
HSRealTurtle <TurtleLuL@mail.ru>
2023-08-09 13:25:49 +0300

    Generate Visual Studio Project
    Create GASCharacter, GASAbilitySystemComponent, GASProjectAttributeSet

---
+++ Source/GASProject/Public/GASProjectAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "GASProjectAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASPROJECT_API UGASProjectAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
